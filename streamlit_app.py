3ceb779d5014bc8927d143bc5ae8e6ebdde70b86
mathildevergnaud <mathildeguyvergnaud@gmail.com>
2026-04-29 16:23:33 +0200

    Added new feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,6 @@ st.title("Image Viewer")
 if selected_image:
     image = st.session_state.images[selected_image]
     st.image(image, caption=selected_image, use_container_width=True)
+    
+    st.subheader("Cornea Segmentation")
 
