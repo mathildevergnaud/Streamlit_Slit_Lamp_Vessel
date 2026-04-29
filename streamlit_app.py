bc7bc0af719301f693bb40a70d3c8ac5dd893d79
mathildevergnaud <mathildeguyvergnaud@gmail.com>
2026-04-29 16:21:24 +0200

    Added new feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,39 @@
 import streamlit as st
-import numpy as np
+from PIL import Image
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+st.set_page_config(layout="wide")
+
+# -------- Sidebar --------
+st.sidebar.title("Upload Images")
+
+uploaded_files = st.sidebar.file_uploader(
+    "Choose images",
+    type=["png", "jpg", "jpeg"],
+    accept_multiple_files=True
+)
+
+# Session state
+if "images" not in st.session_state:
+    st.session_state.images = {}
+
+# Store uploaded images
+if uploaded_files:
+    for file in uploaded_files:
+        if file.name not in st.session_state.images:
+            st.session_state.images[file.name] = Image.open(file)
+
+# Image selector
+st.sidebar.title("Image List")
+image_names = list(st.session_state.images.keys())
+
+selected_image = None
+if image_names:
+    selected_image = st.sidebar.radio("Select an image", image_names)
+
+# -------- Main Layout --------
+st.title("Image Viewer")
+
+if selected_image:
+    image = st.session_state.images[selected_image]
+    st.image(image, caption=selected_image, use_container_width=True)
+
