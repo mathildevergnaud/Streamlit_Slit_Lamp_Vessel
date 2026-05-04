29ee0e6cd0627f6eb0abc7dcd019703a1bcc95dc
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-04 17:10:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if st.sidebar.button("Cornea Segmentation"):
         im = torch.from_numpy(resized_img).permute(2, 0, 1).unsqueeze(0).to(device)
 
         print(im.shape)
-        pred = model(torch.sigmoid(im))[0,0,:,:].cpu().detach().numpy().astype(np.uint8)#*255.0
+        pred = model(im)[0,0,:,:].cpu().detach().numpy().astype(np.uint8)*255.0
         
         # Assuming model is loaded here
         # model = load_model()  # Placeholder for model loading
