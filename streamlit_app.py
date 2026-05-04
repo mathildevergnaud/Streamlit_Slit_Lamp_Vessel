ca61ccb274f922d819244e63f50659680427a999
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-04 17:17:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.cache_resource.clear()
 
 def load_model(device):
     net = build_model().to(device)
-    #net.load_state_dict(torch.load("./utils/cornea/model.pt", map_location=device))
+    net.load_state_dict(torch.load("./utils/cornea/model.pt", map_location=device))
     net.eval()
     return net
 
@@ -56,8 +56,8 @@ if st.sidebar.button("Cornea Segmentation"):
         
         im = torch.from_numpy(resized_img).permute(2, 0, 1).unsqueeze(0).to(device)
 
-        print(im.shape)
-        pred = model(im)[0,0,:,:].cpu().detach().numpy().astype(np.uint8)
+        print(im.shape, im.dtype)
+        pred = (torch.sigmoid(model(im))>0.5).float()[0,0,:,:].cpu().detach().numpy().astype(np.uint8)
         
         # Assuming model is loaded here
         # model = load_model()  # Placeholder for model loading
