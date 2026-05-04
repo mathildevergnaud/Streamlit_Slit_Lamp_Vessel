d8596ebda45544527874b076c81e5d9623cb79ef
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-04 16:47:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,24 @@ from skimage.transform import resize  # assuming this is needed
 st.cache_data.clear()
 st.cache_resource.clear()
 
+def load_model(device):
+    net = build_model().to(device)
+    net.load_state_dict(torch.load("./utils/cornea/model.pt", map_location=device))
+    net.eval()
+    return net
+
+def build_model():
+    return DynUNet(
+        spatial_dims=2,
+        in_channels=3,
+        out_channels=1,
+        kernel_size=[(3, 3)] * 5,
+        strides=[(1, 1), (2, 2), (2, 2), (2, 2), (2, 2)],
+        upsample_kernel_size=[(2, 2)] * 4,
+        norm_name="BATCH",
+        dropout=0.2,
+    )
+
 # Initialize session state
 if "images" not in st.session_state:
     st.session_state.images = {}
