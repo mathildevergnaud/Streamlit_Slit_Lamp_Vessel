0252dab2c50fff5b3e7ae8eeae17e7d195aafd7d
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-05 14:56:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,6 +76,7 @@ if st.sidebar.button("Cornea Segmentation"):
         model = load_model(device)
         
         im = torch.from_numpy(resized_img).permute(2, 0, 1).unsqueeze(0).to(device)
+        
         pred = torch.sigmoid(model(im))[0,0].cpu().detach().numpy()  
         pred = (pred * 255).astype("uint8")
 
@@ -83,7 +84,8 @@ if st.sidebar.button("Cornea Segmentation"):
         pred = encompasse_cornea(pred)
         
         segmented_image = Image.fromarray(pred)
-        st.sidebar.write(np.array(original_image)[0,0], np.array(original_image).dtype, type(np.array(original_image)))
+        
+        st.sidebar.write(np.array(original_image)[0,0], np.array(original_image).dtype, type(np.array(original_image)), pred.dtype, type(pred), pred.max())
         
         st.session_state.segmentations[selected_image_key + "_segmented"] = segmented_image
         st.session_state.segmentations[selected_image_key + "_cornea"] = Cornea_Crop(np.array(original_image), pred)
