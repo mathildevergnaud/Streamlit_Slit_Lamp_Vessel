293e6c105e4a6d51ad5030aeb2d762fc3bbc64c5
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-07 10:53:32 +0200

    Update Cornea.py

--- Cornea.py
+++ Cornea.py
@@ -57,6 +57,7 @@ def run():
     if selected_image_key:
         
         original_image = st.session_state.images[selected_image_key]
+        st.write(type(original_image))
         image = np.array(original_image).astype(np.uint8)
         
         img_array = np.array(original_image).astype(np.float32)/255.0
