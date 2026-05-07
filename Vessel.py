e0e8246de05775da476622034d2124f1e3544cac
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-07 15:34:24 +0200

    Update Vessel.py

--- Vessel.py
+++ Vessel.py
@@ -55,6 +55,56 @@ def flt32_to_unint8(img):
    #print(img.min(),img.max())
     return np.round((((img - img.min())/(img.max()-img.min()) * 255)).astype(np.uint8)) #img.astype(numpy.uint8)#
 
+def cut_im_2(image_in, mask_in, size_im, device = 'cpu'):
+
+    global x_im 
+    global y_im
+    global bordure
+
+    bordure = 100
+        
+	cnt, _ = cv2.findContours((mask_in).astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	contour = max(cnt, key = cv2.contourArea)
+
+	(x,y), radius = cv2.minEnclosingCircle(contour)
+	center = (int(x), int(y))
+	radius = int(radius)
+
+	g = gaussian_2D(center, radius, mask_in.shape)
+
+	g = ((mask_in*g)/255.0).astype(np.float32)
+
+	im = np.array(im, dtype=np.float32)/255.0
+
+	im = np.concatenate((im, g[:,:,None]), axis=2)
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    new_imagette_list=[]
+
+    x_im,y_im,*_  = im.shape 
+    i, j = 0,0
+
+    while i + size_im[0] < x_im :
+        while j + size_im[1] < y_im:
+            new_imagette_list.append(transform(im[i :i+size_im[0], j: j+size_im[1]]).to(device)
+)
+            j+= size_im[1]-bordure
+
+        new_imagette_list.append(transform(im[i :i+size_im[0], y_im-size_im[1]:y_im]).to(device)
+)
+        i += size_im[0]-bordure
+        j = 0
+
+    while j + size_im[1] < y_im:
+        new_imagette_list.append(transform(im[x_im -size_im[0] :x_im, j: j+size_im[1]]).to(device)
+)
+        j+= size_im[1]-bordure
+
+    new_imagette_list.append(transform(im[x_im -size_im[0] :x_im, y_im-size_im[1]:y_im]).to(device)
+)
+
+    return new_imagette_list
+
 
 def run():
     st.title("Vessel Segmentation")
@@ -83,19 +133,19 @@ def run():
             
             st.image(st.session_state.segmentations[selected_image_key + "_mask"], caption="Mask")
 
-
     else :
-        
-        if selected_image_key:
             
-            if selected_image_key:
-                key = selected_image_key + "_cornea"
-                
-                if key in st.session_state.segmentations:
-                    st.write('Cornea Segmentation Done')
-                    st.image(st.session_state.segmentations[selected_image_key + "_cornea"], caption="Cornea")
+		if selected_image_key:
+			key = selected_image_key + "_cornea"
+			
+			if key in st.session_state.segmentations:
+				st.write('Cornea Segmentation Done')
+				st.image(st.session_state.segmentations[selected_image_key + "_cornea"], caption="Cornea")
+				st.session_state.segmentations[selected_image_key + "_mask"] = st.image(st.session_state.segmentations[selected_image_key + "_cornea"]
+	
+			else : 
+				st.write('Please run cornea segmentation before')
 
-                
-    
-                else : 
-                    st.write('Please run cornea segmentation before')
+
+	input = st.st.session_state.images[selected_image_key]
+	mask_in = st.session_state.segmentations[selected_image_key + "_mask"]
