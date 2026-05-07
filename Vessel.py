8badc8bd3dfe71c534f5efea889a3fd9bdbff08d
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-07 13:15:37 +0200

    Update Vessel.py

--- Vessel.py
+++ Vessel.py
@@ -1,17 +1,19 @@
 import streamlit as st
 
 def run():
-    st.title("Cornea Segmentation")
+    st.title("Vessel Segmentation")
 
     if "segmentations" not in st.session_state:
         st.session_state.segmentations = {}
 
-    key = st.text_input("Image key")
+    selected_image_key = st.session_state.get("image_select")
+    st.write(selected_image_key)
+        
+    if selected_image_key:
 
-    if key:
-        seg_key = key + "_cornea"
+        if st.session_state.segmentations[selected_image_key + "_cornea"] :
+            st.write('Cornea Segmentation Done')
 
-        if seg_key in st.session_state.segmentations:
-            st.image(st.session_state.segmentations[seg_key])
-        else:
-            st.info("No cornea result yet.")
+
+        else : 
+            st.write('Missing Cornea Selection')
