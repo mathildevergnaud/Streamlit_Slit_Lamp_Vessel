6eeb1065abad535f091fd9e249fc78ec21d3ce69
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-05-11 15:40:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,61 +94,6 @@ if st.session_state.page == "Vessel":
     else:
         st.warning("Please upload and select an image first.")
 
-# col1, col2, col3 = st.columns([1, 1, 6])
-
-# with col1:
-#     if st.button("🏠 Main", use_container_width=True):
-#         st.write('main')
-#         #st.switch_page("main.py")
-
-# with col2:
-#     if st.button("Cornea Segmentation", on_click=set_page, args=("cornea",),use_container_width=True):
-#         if st.session_state.page == "cornea":
-#             cornea.run()
-
-# with col3:
-#     if st.button("Vessel Segmentation", on_click=set_page, args=("vessel",),use_container_width=True):
-#         if st.session_state.page == "vessel":
-#             vessel.run()
-    
-    #     # st.session_state.vessel_mode = "menu"
-    #     # st.sidebar.write("Choose option:")
-        
-    #     # if st.sidebar.button("Option A"):
-    #     #     st.session_state.vessel_mode = "A"
-    
-    #     # if st.sidebar.button("Option B"):
-    #     #     st.session_state.vessel_mode = "B"
-
-    #     # if st.session_state.vessel_mode == "A":
-    #     #     st.sidebar.write("Please upload images")
-    #     def run():
-    # st.title("Cornea Segmentation")
-
-    # if "segmentations" not in st.session_state:
-    #     st.session_state.segmentations = {}
-
-    # key = st.text_input("Image key")
-
-    # if key:
-    #     seg_key = key + "_cornea"
-
-    #     if seg_key in st.session_state.segmentations:
-    #         st.image(st.session_state.segmentations[seg_key])
-    #     else:
-    #         st.info("No cornea result yet.")
-    #     #     uploaded_file = st.sidebar.file_uploader(
-    #     #         "Upload image",
-    #     #         type=["jpg", "jpeg", "png"],
-    #     #         key="Mask_upload"
-    #     #     )
-        
-    #     #     if uploaded_file is not None:
-    #     #         mask = Image.open(uploaded_file)
-    #     #         st.session_state.segmentations[selected_image_key + "_Mask"] = mask
-    #     #         st.success("Mask saved")
-
-
 
     # st.image(image, caption=selected_image, use_container_width=True)
     
