9fea9a4144b2d3212e21b4703dd591c3e081716f
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-08-07 16:37:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,7 @@ with tab_single:
                         st.session_state.segmentations[selected_image_key + "_mask"] = mask
     
                         np_mask = np.array(mask).astype(np.float32)/255.0
-                        np_or = np.array(st.session_state.segmentations[selected_image_key+'_or']).astype(np.uint8)
+                        np_or = np.array(st.session_state.segmentations[selected_image_key+"_or"]).astype(np.uint8)
                         
                         st.session_state.segmentations[selected_image_key + "_cornea"] = Image.fromarray(cornea.Cornea_crop(np_or,np_mask))
                         st.session_state.cornea_done = True
