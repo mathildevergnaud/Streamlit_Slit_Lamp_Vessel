728f75c5a8f56c729b0529f9939413c62b252a8b
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-08-08 10:58:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ with tab_single:
                         np_mask = np.array(mask)#.astype(np.uint8)
                         np_or = np.array(st.session_state.images[selected_image_key + "_or"]).astype(np.uint8)
                         
-                        st.session_state.segmentations[selected_image_key + "_cornea"] = Image.fromarray(fct.Cornea_Crop(np_mask,np_or))
+                        st.session_state.segmentations[selected_image_key + "_cornea"] = Image.fromarray(fct.Cornea_Crop(np_or,np_mask))
                         st.session_state.cornea_done = True
     
                         if selected_image_key and selected_image_key + "_mask" in st.session_state.segmentations:
