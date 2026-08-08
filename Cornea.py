b843bb87e7829868f4393b737f0f21c06cd61636
Mathilde Vergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2026-08-08 11:32:15 +0200

    Update Cornea.py

--- Cornea.py
+++ Cornea.py
@@ -63,7 +63,7 @@ def run(selected_image_key):
     if selected_image_key:
         
         original_image = st.session_state.images[selected_image_key+'_or']
-        st.image(original_image, caption="Segmented Image")
+        #st.image(original_image, caption="Segmented Image")
         
         np_image = np.array(original_image).astype(np.uint8)
         img_array = np.array(original_image).astype(np.float32)/255.0
